afcd2b029c123fef70874ab2da32b7777da117a8
Øystein Myrmo <oystein.myrmo@gmail.com>
2019-02-25 22:11:27 +0100

    Initial LRUDiskCache with test

--- include/myrmo/cache/disk.h
+++ include/myrmo/cache/disk.h
@@ -20,86 +20,321 @@
  */
 #pragma once
 #include <string>
+#include <vector>
 #include <cstdint>
 #include <cstdio>
 #include <fstream>
+#include <list>
+#include <algorithm>
 
 namespace myrmo { namespace cache
 {
-	// TODO: Support streaming of large files, both readn and write.
-	class DiskCache
+	// TODO: Support streaming of large files, both read and write.
+	class LRUDiskCache
 	{
 	public:
+		enum class Error : unsigned int
+		{
+			NoError,
+			FileDoesNotExist,
+			FileSizeGreaterThanMaxCacheSize,
+			FileExists,
+			CouldNotDeleteFile,
+			CouldNotClearSpaceForFile,
+			CouldNotWriteFile,
+			CouldNotWriteIndexFile
+		};
+
 		typedef std::string (*hashFunction)(const std::string& uri);
 
-		DiskCache() = delete;
+		LRUDiskCache() = delete;
+		LRUDiskCache(const LRUDiskCache& cache) = delete;
 
-		DiskCache(const std::string& cacheDir, hashFunction func)
+		LRUDiskCache(const std::string& cacheDir, hashFunction func, size_t cacheSizeInMegaBytes = 50)
 			: mCacheDir(cacheDir)
 			, mHashFunction(func)
-		{}
-
-		// TODO: Take in std::vector instead!
-		bool read(const std::string& uri, char** data, size_t* size) const
+			, mMaxCacheSize(cacheSizeInMegaBytes * 1048576)
+			, mCacheSize(0)
 		{
-			bool couldRead = false;
-			std::ifstream f(filename(uri), std::ios::binary);
+			Error error = readIndexFile();
+			assert(error == Error::NoError);
+		}
+
+		~LRUDiskCache()
+		{
+			Error error = writeIndexFile();
+			assert(error == Error::NoError);
+		}
+
+		Error read(const std::string& uri, std::vector<char>* data)
+		{
+			Error error = Error::FileDoesNotExist;
+			const std::string hash(mHashFunction(uri));
+			std::ifstream f(file_path(hash), std::ios::binary);
 
 			if (f.is_open())
 			{
 				f.seekg(0, std::ios::end);
-				size_t fSize = f.tellg();
+				const size_t fSize = f.tellg();
 				f.seekg(0, std::ios::beg);
 
-				*data = new char[fSize]; // TODO: Fill std::vector!
-				*size = fSize;
-				f.read(*data, fSize);
+				data->resize(fSize, '\0');
+				f.read(&(*data)[0], fSize);
 				f.close();
-				couldRead = true;
+
+				auto it = std::find(mLRUData.begin(), mLRUData.end(), hash);
+				assert(it != mLRUData.end()); // The item must exist in the LRU structure when we could read it from disk.
+				if (it != mLRUData.end())
+					mLRUData.splice(mLRUData.begin(), mLRUData, it); // Move item to front
+
+				error = Error::NoError;
 			}
 
-			return couldRead;
+			return error;
 		}
 
-		bool write(const std::string uri, const std::string& data) const
+		Error write(const std::string& uri, const char* data, size_t size)
+		{
+			Error error = Error::NoError;
+			const std::string hash(mHashFunction(uri));
+			const std::string fName(file_path(hash));
+
+			std::ifstream i(fName, std::ios::binary);
+			if (i.good())
+			{
+				error = Error::FileExists;
+				i.close();
+			}
+			else
+			{
+				std::ofstream f(file_path(hash), std::ios::binary);
+				if (f.is_open())
+				{
+					error = evictUntilEnoughSpace(size);
+					if (error == Error::NoError)
+					{
+						f.write(data, size);
+						mCacheSize += size;
+						mLRUData.push_front(hash);
+						error = writeIndexFile();
+					}
+					f.close();
+				}
+				else
+				{
+					error = Error::CouldNotWriteFile;
+				}
+			}
+
+			return error;
+		}
+
+		Error write(const std::string& uri, const std::string& data)
 		{
 			return write(uri, data.c_str(), data.size());
 		}
 
-		bool write(const std::string uri, const char* data, size_t size) const
+		Error clear()
 		{
-			bool couldWrite = false;
-			std::ofstream f(filename(uri), std::ios::binary);
+			Error error = Error::NoError;
 
-			if (f.is_open())
+			// Copy the list because removeFile alters mLRUData.
+			std::vector<std::string> copy;
+			copy.reserve(mLRUData.size());
+			for (const auto& it : mLRUData)
+				copy.emplace_back(it);
+
+			for (const auto& item : copy)
 			{
-				f.write(data, size);
-				f.close();
-				couldWrite = true;
+				error = removeFile(item);
+				if (error != Error::NoError)
+					break;
 			}
 
-			return couldWrite;
+			if (error == Error::NoError)
+			{
+				assert(mLRUData.size() == 0);
+				error = removeIndexFile();
+				mCacheSize = 0;
+			}
+
+			return error;
 		}
 
-		int remove(const std::string& uri) const
+		inline Error remove(const std::string& uri)
 		{
-			int result = 0;
-			std::string fileName = filename(uri.c_str());
-			std::ifstream f(fileName);
-			if (f.good())
-				result = std::remove(filename(uri).c_str());
-			return result;
+			Error error = removeFile(mHashFunction(uri));
+			if (error == Error::NoError)
+				writeIndexFile();
+			return error;
+		}
+
+		size_t size() const
+		{
+			return mCacheSize; // Disregarding index file
+		}
+
+		size_t count() const
+		{
+			return mLRUData.size(); // Disregarding index file
 		}
 
 	private:
-		std::string filename(const std::string& uri) const
+		inline Error removeFile(const std::string& hash)
 		{
-			return mCacheDir + "/" + mHashFunction(uri);
+			Error error = Error::NoError;
+
+			const std::string fileName = file_path(hash);
+			std::ifstream f(fileName, std::ios::binary);
+			if (f.good())
+			{
+				f.seekg(0, std::ios::end);
+				const size_t fSize = f.tellg();
+				f.seekg(0, std::ios::beg);
+
+				bool removed = std::remove(fileName.c_str()) == 0;
+				if (removed)
+				{
+					mCacheSize -= fSize;
+					auto it = std::find(mLRUData.begin(), mLRUData.end(), hash);
+					if (it != mLRUData.end()) // The index file is not in the mLRUData, so we must allow it != mLRUData.end()
+						mLRUData.erase(it);
+				}
+				else
+				{
+					error = Error::CouldNotDeleteFile;
+				}
+			}
+			else
+			{
+				error = Error::FileDoesNotExist;
+			}
+
+			return error;
+		}
+
+		inline std::string file_path(const std::string& hash) const
+		{
+			return mCacheDir + "/" + hash;
+		}
+
+		inline Error writeIndexFile() const
+		{
+			Error error = Error::CouldNotWriteIndexFile;
+
+			const std::string hash(mHashFunction("myrmo_lru_cache_data"));
+			const std::string fName = mCacheDir + "/" + hash;
+			std::ofstream f(fName, std::ios::binary);
+
+			if (f.is_open())
+			{
+				const size_t hashSize = hash.size();
+				for (auto it = mLRUData.begin(); it != mLRUData.end(); it++)
+				{
+					assert(it->size() == hashSize); // If not the cache would be corrupted.
+					f.write(it->c_str(), hashSize);
+				}
+
+				f.close();
+				error = Error::NoError;
+			}
+
+			return error;
+		}
+
+		inline Error readIndexFile()
+		{
+			Error error = Error::NoError;
+
+			const std::string hash(mHashFunction("myrmo_lru_cache_data"));
+			const std::string fName = file_path(hash);
+			std::ifstream f(fName, std::ios::binary);
+
+			if (f.is_open())
+			{
+				const size_t hashSize = hash.size();
+
+				f.seekg(0, std::ios::end);
+				const size_t fSize = f.tellg();
+				assert((fSize % hashSize) == 0); // Otherwise the data is corrupted
+				f.seekg(0, std::ios::beg);
+
+				size_t pos = 0;
+				while (pos < fSize)
+				{
+					std::string element;
+					element.resize(hashSize, '\0');
+					f.read(&element[0], hashSize);
+
+					std::ifstream currentFile(file_path(element), std::ifstream::ate | std::ifstream::binary);
+					if (currentFile.is_open())
+					{
+						mCacheSize += currentFile.tellg();
+						mLRUData.emplace_back(element);
+					}
+
+					currentFile.close();
+					pos += hashSize;
+				}
+
+				f.close();
+			}
+
+			return error;
+		}
+
+		inline Error removeIndexFile()
+		{
+			const std::string hash(mHashFunction("myrmo_lru_cache_data"));
+			removeFile(hash);
+			return Error::NoError;
+		}
+
+		inline Error evictUntilEnoughSpace(size_t size)
+		{
+			Error error = Error::NoError;
+
+			size_t errorCount = 0;
+			while ((mCacheSize + size) > mMaxCacheSize)
+			{
+				if (mLRUData.empty())
+				{
+					error = Error::FileSizeGreaterThanMaxCacheSize;
+					assert(mCacheSize == 0); // We should not end up here unless mCacheSize is 0.
+					break;
+				}
+				else
+				{
+					const std::string& hash = mLRUData.back();
+					error = removeFile(hash);
+					assert(error == Error::NoError); // The cache is corrupt if we end up removing files that does not exist.
+					if (error == Error::NoError)
+					{
+						errorCount = 0;
+						mLRUData.pop_back();
+					}
+					else
+					{
+						if (++errorCount > 5)
+						{
+							assert(false); // Tried to remove on file 5 times and failed. Cannot tell why.
+							error = Error::CouldNotClearSpaceForFile;
+							break;
+						}
+					}
+				}
+			}
+
+			return error;
 		}
 
 	private:
 		std::string  mCacheDir;
 		hashFunction mHashFunction;
+
+		std::list<std::string> mLRUData; // Least Recenty Used is at the end.
+		const size_t mMaxCacheSize;
+		size_t mCacheSize; // In megabytes
 	};
-	
+
 }} // End namespace myrmo::cache
--- tests/cache/disk-cache-tests.cpp
+++ tests/cache/disk-cache-tests.cpp
@@ -6,47 +6,182 @@
 #include <cstdio>
 #include <array>
 
-// Simulating some file data.
-struct FileData
+#include <cmrc/cmrc.hpp>
+
+CMRC_DECLARE(test_data);
+
+static std::string get_file(const std::string &name)
 {
-	std::string url;
-	std::string data;
+	auto fs = cmrc::test_data::get_filesystem();
+	auto file = fs.open(name);
+	return std::string(file.begin(), file.size());
+}
+
+struct ImageTestData
+{
+	std::string name;
+	int size; // in bytes
 };
 
-std::array<FileData, 5> fileData {{
-	{ "https://www.miasmat.no/wp-content/uploads/app/w240/135.JPG",  "135_w240"  },
-	{ "https://www.miasmat.no/wp-content/uploads/app/w480/135.JPG",  "135_w480"  },
-	{ "https://www.miasmat.no/wp-content/uploads/app/w800/135.JPG",  "135_w800"  },
-	{ "https://www.miasmat.no/wp-content/uploads/app/w1080/135.JPG", "135_w1080" },
-	{ "https://www.miasmat.no/wp-content/uploads/app/w1280/135.JPG", "135_w1280" }
+constexpr size_t IMAGE_COUNT = 23;
+std::array<ImageTestData, IMAGE_COUNT> images {{
+	{ "test_data/122.JPG", 332004 },
+	{ "test_data/125.JPG", 323953 },
+	{ "test_data/115.JPG", 314677 },
+	{ "test_data/114.JPG", 306891 },
+	{ "test_data/120.JPG", 267281 },
+	{ "test_data/121.JPG", 249116 },
+	{ "test_data/124.JPG", 243953 },
+	{ "test_data/128.JPG", 240013 },
+	{ "test_data/111.JPG", 222219 },
+	{ "test_data/103.JPG", 214214 },
+	{ "test_data/123.JPG", 206680 },
+	{ "test_data/107.JPG", 205015 },
+	{ "test_data/108.JPG", 201858 },
+	{ "test_data/112.JPG", 198872 },
+	{ "test_data/118.JPG", 196191 },
+	{ "test_data/106.JPG", 185726 },
+	{ "test_data/117.JPG", 179626 },
+	{ "test_data/110.JPG", 174580 },
+	{ "test_data/109.JPG", 170556 },
+	{ "test_data/126.JPG", 154536 },
+	{ "test_data/116.JPG", 146006 },
+	{ "test_data/119.JPG", 138155 },
+	{ "test_data/113.JPG", 136281 }
 }};
 
-void clearCache(myrmo::cache::DiskCache& diskCache)
+size_t allImagesSize()
 {
-	for (const auto& f : fileData)
+	size_t size = 0;
+	for (size_t i = 0; i < IMAGE_COUNT; i++)
+		size += images[i].size;
+	return size;
+}
+
+myrmo::cache::LRUDiskCache::Error imageExists(myrmo::cache::LRUDiskCache& cache, size_t pos, std::vector<char>* data)
+{
+	std::string file(images[pos].name);
+	return cache.read(images[pos].name, data);
+}
+
+myrmo::cache::LRUDiskCache::Error insertImage(myrmo::cache::LRUDiskCache& cache, size_t pos)
+{
+	std::string file(images[pos].name);
+	std::string data(get_file(file));
+	return cache.write(file, data);
+}
+
+myrmo::cache::LRUDiskCache::Error deleteImage(myrmo::cache::LRUDiskCache& cache, size_t pos)
+{
+	return cache.remove(images[pos].name);
+}
+
+void test_insert_read_delete_all_images()
+{
+	using namespace myrmo::cache;
+
 	{
-		MYRMO_ASSERT(diskCache.remove(f.url) == 0);
+		LRUDiskCache cache(MYRMO_TESTS_CACHE_DIR, myrmo::hash::sha1);
+		std::vector<char> data;
+
+		// Assert no images exist.
+		for (size_t i = 0; i < IMAGE_COUNT; i++)
+			MYRMO_ASSERT(imageExists(cache, i, &data) == LRUDiskCache::Error::FileDoesNotExist);
+
+		// Assert insertion of all images ok.
+		for (size_t i = 0; i < IMAGE_COUNT; i++)
+			MYRMO_ASSERT(insertImage(cache, i) == LRUDiskCache::Error::NoError);
+
+		// Assert all images can be fetched.
+		for (size_t i = 0; i < IMAGE_COUNT; i++)
+			MYRMO_ASSERT(imageExists(cache, i, &data) == LRUDiskCache::Error::NoError);
+
+		// Assert cache size/count.
+		MYRMO_ASSERT(cache.size() == allImagesSize());
+		MYRMO_ASSERT(cache.count() == IMAGE_COUNT);
+
+		// Assert bytes are ok.
+		for (size_t i = 0; i < IMAGE_COUNT; i++)
+		{
+			std::string image(get_file(images[i].name));
+			MYRMO_ASSERT(cache.read(images[i].name, &data) == LRUDiskCache::Error::NoError);
+			MYRMO_ASSERT(image.size() == data.size());
+			MYRMO_ASSERT(memcmp(image.data(), data.data(), data.size()) == 0);
+		}
 	}
+
+	{
+		LRUDiskCache cache(MYRMO_TESTS_CACHE_DIR, myrmo::hash::sha1);
+		std::vector<char> data;
+
+		// Assert all images can be fetched.
+		for (size_t i = 0; i < IMAGE_COUNT; i++)
+			MYRMO_ASSERT(imageExists(cache, i, &data) == LRUDiskCache::Error::NoError);
+
+		// Assert cache size/count;
+		MYRMO_ASSERT(cache.size() == allImagesSize());
+		MYRMO_ASSERT(cache.count() == IMAGE_COUNT);
+
+		// Asert bytes are ok.
+		for (size_t i = 0; i < IMAGE_COUNT; i++)
+		{
+			std::string image(get_file(images[i].name));
+			MYRMO_ASSERT(cache.read(images[i].name, &data) == LRUDiskCache::Error::NoError);
+			MYRMO_ASSERT(image.size() == data.size());
+			MYRMO_ASSERT(memcmp(image.data(), data.data(), data.size()) == 0);
+		}
+
+		// Remove every second image.
+		size_t removedSize = 0;
+		for (size_t i = 0; i < IMAGE_COUNT; i += 2)
+		{
+			MYRMO_ASSERT(deleteImage(cache, i) == LRUDiskCache::Error::NoError);
+			removedSize += images[i].size;
+		}
+
+		// Assert cache size/count;
+		MYRMO_ASSERT(cache.count() == (IMAGE_COUNT / 2));
+		MYRMO_ASSERT(cache.size() == (allImagesSize() - removedSize));
+
+		// Check existance / verify data.
+		for (size_t i = 0; i < IMAGE_COUNT; i++)
+		{
+			if ((i % 2) == 0)
+			{
+				MYRMO_ASSERT(imageExists(cache, i, &data) == LRUDiskCache::Error::FileDoesNotExist);
+			}
+			else
+			{
+				std::string image(get_file(images[i].name));
+				MYRMO_ASSERT(cache.read(images[i].name, &data) == LRUDiskCache::Error::NoError);
+				MYRMO_ASSERT(image.size() == data.size());
+				MYRMO_ASSERT(memcmp(image.data(), data.data(), data.size()) == 0);
+			}
+		}
+	}
+
+	LRUDiskCache cache(MYRMO_TESTS_CACHE_DIR, myrmo::hash::sha1);
+	MYRMO_ASSERT(cache.clear() == LRUDiskCache::Error::NoError);
+	MYRMO_ASSERT(cache.size() == 0);
+	MYRMO_ASSERT(cache.count() == 0);
+}
+
+void test_disk_cache_eviction_policy()
+{
+	// 1. Create small disk cache. Add files so that the maximum size is exceeded.
+	// 2. Check that the correct files and number of files exists.
+	// 3. Verify LRU order.
+	// 4. Do multiple times: a) get a file and b) check LRU order
 }
 
 int main()
 {
-	std::string cacheDir(MYRMO_TESTS_CACHE_DIR);
-	myrmo::cache::DiskCache diskCache(cacheDir, myrmo::hash::sha1);
-
-	for (const auto& f : fileData)
 	{
-		diskCache.write(f.url, f.data.c_str(), f.data.size());
+		using namespace myrmo::cache;
+		LRUDiskCache cache(MYRMO_TESTS_CACHE_DIR, myrmo::hash::sha1);
+		MYRMO_ASSERT(cache.clear() == LRUDiskCache::Error::NoError);
 	}
 
-	for (const auto& f : fileData)
-	{
-		char* data;
-		size_t size;
-		diskCache.read(f.url, &data, &size);
-		MYRMO_ASSERT(size == f.data.size())
-		MYRMO_ASSERT(memcmp(data, f.data.data(), size) == 0);
-	}
-	
+	test_insert_read_delete_all_images();
 	return 0;
 }
